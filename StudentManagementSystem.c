75a1aef8efa9c247ffd06629f391715335f6986a
ShubhamKumarRaman <shubham620409@gmail.com>
2025-02-28 13:59:15 +0530

    Add features

--- StudentManagementSystem.c
+++ StudentManagementSystem.c
@@ -1,10 +1,11 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define FILENAME "students.dat"
 
-struct Student{
+struct Student
+{
     int id;
     char name[50];
     int age;
@@ -17,6 +18,20 @@ void searchStudent();
 void deleteStudent();
 void updateStudent();
 
-int main(){
+int main()
+{
+    int choice;
+    while (1)
+    {
+        pirntf("\nStudent Management System\n");
+        pritnf("1. Add Student\n");
+        printf("2. Display Students\n");
+        printf("3. Search Student\n");
+        printf("4. Delete Student\n");
+        printf("5. Update Student\n");
+        printf("6. Exit\n");
+        printf("\nEnter your choice: ");
+        scanf("%d", &choice);
+    }
     return 0;
 }
